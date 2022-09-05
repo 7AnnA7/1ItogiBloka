eab4b0e6249b3e1f625b7d745f160258cbf38a88
AnnA <anna_elfimova@mail.ru>
2022-09-05 15:02:11 +0300

    1ItogBloka massiv iz strok dlinoi menshe libo ravno 3 simvola

---
+++ 1ItogBloka/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк,
+// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры,
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
+// лучше обойтись исключительно массивами.
+
+
+Console.WriteLine("Введите строки массива через пробел: \n");
+string text = Console.ReadLine();
+string[] massString = text.Split(' ');
+
+var res = from word in massString
+          where word.Length <= 3
+          select word;
+if (res.Count() == 0)
+{
+    Console.WriteLine("Нет строк, длина которых меньше либо равна 3 символа");
+}
+else
+{
+    Console.Write("\nМассив из строк, длина которых меньше либо равна 3 символа -> [ ");
+    for (int i = 0; i < massString.Length; i++)
+    {
+        if (massString[i].Length <= 3)
+            Console.Write(massString[i] + " ");
+    }
+    Console.Write(']');
+}
